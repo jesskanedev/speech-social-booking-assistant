7a56e2d15e8e9e97e94d4771a8552e13961fdb05
Jessica Kane <JessicaK@intergen.org.nz>
2020-03-14 17:43:15 +1300

    Add front end control, have another go at neural voice SSML

---
+++ bot/Helpers/VoiceExpressionOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreBot.Helpers
+{
+    public static class VoiceExpressionOptions
+    {
+        public static string Cheerful = "cheerful"; //Expresses an emotion that is positive and happy
+        public static string Empathy = "empathy"; //Expresses a sense of caring and understanding
+        public static string Chat = "chat"; //Speak in a casual, relaxed tone
+        public static string Newscast = "newscast"; //Expresses a formal tone, similar to news broadcasts
+        public static string CustomerService = "customerservice"; //Speak in a friendly and patient way as customer service
+    }
+}
--- bot/Helpers/VoiceMessageHelpers.cs
+++ bot/Helpers/VoiceMessageHelpers.cs
@@ -1,10 +1,16 @@
-﻿namespace CoreBot.Helpers
+﻿using System.Text;
+
+namespace CoreBot.Helpers
 {
     /// <summary>
     /// Voice message helper functions.
     /// </summary>
     public static class VoiceMessageHelpers
     {
+        private static string _voiceRegion = "en-US";
+        private static string _voiceName = "JessaNeural";
+        private static readonly string _voiceExpressAs = VoiceExpressionOptions.CustomerService;
+
         /// <summary>
         /// Wrap the message with a neural voice for more realistic text to speech.
         /// Standard voices: https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#standard-voices
@@ -15,13 +21,22 @@
         public static string WrapMessageInVoice(string message)
         {
             // No custom voice
-            var voiceMessage = message;
+            //var voiceMessage = message;
 
             //Standard custom voice
-            //var voiceMessage = $"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='Microsoft Server Speech Text to Speech Voice (en-US, Jessa24kRUS)'>{message}</voice></speak>";
+            //var voiceMessage = $"<speak version=\"1.0\" xmlns=\"https://www.w3.org/2001/10/synthesis\" xml:lang=\"{_voiceRegion}\"><voice name=\"Microsoft Server Speech Text to Speech Voice ({_voiceRegion}, Jessa24kRUS)\">{message}</voice></speak>";
 
             //Neural custom voice - breaks???
-//            var voiceMessage = $"<speak version='1.0' xmlns='https://www.w3.org/2001/10/synthesis' xml:lang='en-US'><voice name='Microsoft Server Speech Text to Speech Voice (en-US, JessaNeural)'><mstts:express-as type='chat'>{message}</mstts:express-as></voice></speak>";
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append($"<speak version=\"1.0\" xmlns=\"https://www.w3.org/2001/10/synthesis\" xml:lang=\"{_voiceRegion}\">");
+            stringBuilder.Append($"<voice name=\"Microsoft Server Speech Text to Speech Voice ({_voiceRegion}, {_voiceName})\">");
+            stringBuilder.Append($"<mstts:express-as type=\"{_voiceExpressAs}\">");
+            stringBuilder.Append($"{message}");
+            stringBuilder.Append("</mstts:express-as>");
+            stringBuilder.Append("</voice>");
+            stringBuilder.Append("</speak>");
+
+            var voiceMessage = stringBuilder.ToString();
 
             return voiceMessage;
         }
